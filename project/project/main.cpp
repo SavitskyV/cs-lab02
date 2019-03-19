de03cf24315822c671a5cfc7a3b3d5abab779a0c
Alice (SavitskyV) <s@v.com>
2019-03-19 14:57:53 +0300

    code: заготовка программы

---
+++ project/project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Valentin Savitsky on 3/19/19.
+//  Copyright © 2019 Valentin Savitsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
