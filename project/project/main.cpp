6a2188900c6a52c470d340d887ee08c41da65793
Alice (SavitskyV) <s@v.com>
2019-04-02 05:52:57 +0300

    code: добавил деление чисел

--- project/project/main.cpp
+++ project/project/main.cpp
@@ -8,4 +8,5 @@ int main() {
     cout << "A + B = " << a + b << '\n';
     cout << "A - B = " << a - b << '\n';
     cout << "A * B = " << a * b << '\n';
+    cout << "A / B = " << a / b << '\n';
 }
